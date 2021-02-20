dd02189c45def6a1898450cc0f890e6f7900d2ef
NSParadox <76891137+NSParadox@users.noreply.github.com>
2021-02-20 15:16:16 -0600

    Beginning game

---
+++ Frogger/Assets/Scripts/Frog.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Frog : MonoBehaviour
+{
+    public Rigidbody2D frogrb;
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            frogrb.MovePosition(frogrb.position + Vector2.right);
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            frogrb.MovePosition(frogrb.position + Vector2.left);
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            frogrb.MovePosition(frogrb.position + Vector2.up);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            frogrb.MovePosition(frogrb.position + Vector2.down);
+        }
+    }
+}
